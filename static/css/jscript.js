10f4b8f8ffbec3b84b3ef4f5811ab718b4d0ae76
siva4334 <64244158+siva4334@users.noreply.github.com>
2020-05-29 00:01:33 +0530

    Update jscript.js

--- static/css/jscript.js
+++ static/css/jscript.js
@@ -1,19 +1,20 @@
 
-$('a').slice(0,5).on('mouseover',function(){
+$('a').slice(0,6).on('mouseover',function(){
 	$(this).css({color:'rgb(250,5,5)'})
 });
 
-$('a').slice(0,5).on('mouseout',function(){
+$('a').slice(0,6).on('mouseout',function(){
 	$(this).css({color:'white'})
 });
 
 
 
-$('a').slice(5,1000).on('mouseover',function(){
+$('a').slice(6,1000).on('mouseover',function(){
 	$(this).css({color:'red'})
 });
 
-$('a').slice(5,1000).on('mouseout',function(){
+$('a').slice(6,1000).on('mouseout',function(){
 	$(this).css({color:'black'})
 });
+
 
